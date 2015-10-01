0103de5bb70f75cf741851eecb4ef36c88fd1832
Gilles Wittenberg <git@gilleswittenberg.com>
2015-10-01 23:28:15 +0200

    Mithril

--- js/app.js
+++ js/app.js
@@ -1,75 +1,125 @@
 
 
-var ip = '54.93.181.197';
+//var url = 'http://54.93.181.197:7676/';
+var url = 'http://localhost:7676/';
 
-var idMe;
-var name = 'Gilles Wittenberg';
+// components
+var Items = {};
 
-var url = 'http://' + ip + ':7474/db/data/transaction/commit';
-// @TODO: Fix for when user has no children yet
-var query = "MATCH (user:User {name: '" + name + "'})-->(children) return user, ID(user), children, ID(children)";
-var params = {limit: 10};
-var json = {
-  statements: [
-    {
-      statement: query,
-      parameters: params
+// model
+Items.Model = function (data) {
+
+  data = data || {};
+  this.id = m.prop(data.id);
+  this.parentId = m.prop(data.parentId);
+  this.parent = m.prop(data.parent);
+  this.children = m.prop([]);
+  if (data.children) {
+    data.children.forEach(function (data) {
+      data.parent = this;
+      data.parentId = this.id();
+      this.children().push(new Items.Model(data));
+    }.bind(this));
+  }
+  this.name = m.prop(data.name)
+  this.text = m.prop(data.text)
+  this.vm = new Items.ViewModel(this);
+};
+Items.Model.prototype.del = function () {
+
+  this.parent().removeChild(this.id());
+
+  return m.request({
+    method: 'DELETE',
+    url: url + 'nodes/' + this.id()
+  });
+};
+Items.Model.prototype.save = function () {
+
+  var method, data = {};
+
+  if (!this.id()) {
+    method = 'POST';
+    data.parentId = this.parentId();
+  } else {
+    method = 'PUT';
+  }
+  data.text = this.text;
+
+  return m.request({
+    method: method,
+    url: url + 'nodes/',
+    data: data
+  });
+};
+Items.Model.prototype.createChild = function (text) {
+  var child = new Items.Model({
+    parent: this,
+    parentId: this.id(),
+    text: text
+  });
+
+  this.children().push(child);
+
+  return child.save();
+};
+Items.Model.prototype.removeChild = function (id) {
+  this.children().forEach(function (child, index) {
+    if (child.id() === id) {
+      this.children().splice(index, 1);
     }
-  ]
+  }.bind(this));
+}
+
+// controller
+Items.Controller = function () {};
+
+// view
+Items.View = {};
+Items.View.Items = function (ctrl) {
+
+  return [
+    m('h1', Items.Root.name()),
+    m('div', {className: 'wrapper'}, [
+      Items.Root.children().map(Items.View.Item.bind(ctrl))
+    ])
+  ];
 };
 
-lib.request(url, json).then(renderAll);
+Items.View.Item = function (model) {
 
-function renderAll (json) {
+  return m('ul', [
+    m('li', {className: ''}, [
+      m('span', model.text()),
+      m('button.delete', {onclick: model.del.bind(model)}, 'X'),
+      model.children().map(Items.View.Item.bind(this)),
+      m('form', {onsubmit: model.vm.add.bind(model.vm)}, [
+        m('input[type=text]', {onchange: m.withAttr('value', model.vm.val), value: model.vm.val()}),
+        m('input[type=submit]', {value: '+'})
+      ])
+    ])
+  ]);
+};
 
-  idMe = json.results[0].data[0].row[1];
-
-  renderH1(json.results[0]);
-  renderRows(json.results[0]);
-}
-
-var h1 = document.getElementsByTagName('h1')[0];
-function renderH1 (json) {
-  h1.innerHTML = json.data[0].row[0].name;
-}
-
-function renderRows (json) {
-  var rows = json.data;
-  rows.forEach(function (row) {
-    renderRow(row.row[3], row.row[2].text);
-  });
-}
-
-var ul = document.getElementsByTagName('ul')[0];
-function renderRow (id, text) {
-  var item = new Item(id, text);
-  ul.appendChild(item.elem);
-}
-
-
-// add
-var form = document.getElementsByTagName('form')[0];
-var input = document.getElementsByTagName('input')[0];
-form.addEventListener('submit', function (event) {
-
+Items.ViewModel = function (model) {
+  this.val = m.prop('');
+  this.model = m.prop(model);
+};
+Items.ViewModel.prototype.add = function (event) {
   event.preventDefault();
+  if (this.val() !== '') {
+    this.model().createChild(this.val());
+    this.val('');
+  }
+};
 
-  var text = input.value;
-  var query = "MATCH (user: User {name: '" + name + "'}) CREATE (node:Text {text: '" + text + "'})<-[r:Child]-(user) RETURN node, ID(node)";
-  var json = {
-    statements: [
-      {
-        statement: query,
-      }
-    ]
-  };
-  lib.request(url, json).then(function (response) {
-
-    // clear input
-    input.value = '';
-
-    // add row
-    var row = response.results[0].data[0].row;
-    renderRow(row[1], row[0].text);
-  });
+// initialize
+m.request({
+  method: 'GET',
+  url: url + 'nodes'
+}).then(function (response) {
+  Items.Root = new Items.Model(response);
+  m.mount(document.body, {controller: Items.Controller, view: Items.View.Items});
 });
+
+
