f0b19304fecb31bc287ad457424baf42bd37d394
Gilles Wittenberg <git@gilleswittenberg.com>
2015-10-04 15:59:16 +0200

    Gulp jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
+
+/* jshint globalstrict: true */
+/* global require */
 'use strict';
 
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 
+var jsFiles = ['gulpfile.js', './js/*.js'];
+var sassFiles = './sass/**/*.scss';
+
+gulp.task('jshint', function () {
+  gulp.src(jsFiles)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
 gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
+  gulp.src(sassFiles)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css/'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('watch', function () {
+  gulp.watch(jsFiles, ['jshint']);
+  gulp.watch(sassFiles, ['sass']);
 });
